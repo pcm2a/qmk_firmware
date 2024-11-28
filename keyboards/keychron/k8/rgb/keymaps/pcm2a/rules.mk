8cfc27655fcafb544b2fd47f0c83eaaa101e3747
Cameron McBride <c@c.com>
2024-11-28 10:15:21 -0600

    Updates

--- keyboards/keychron/k8/rgb/keymaps/pcm2a/rules.mk
+++ keyboards/keychron/k8/rgb/keymaps/pcm2a/rules.mk
@@ -1,9 +1,6 @@
 OPENRGB_ENABLE = no
 MOUSEKEY_ENABLE = yes
 OS_DETECTION_ENABLE = yes
-VIA_ENABLE = yes
+VIA_ENABLE = no
 DYNAMIC_MACRO_ENABLE = yes
 TAP_DANCE_ENABLE = no
-
-VPATH += keyboards/keychron/common
-SRC += keychron_common.c
