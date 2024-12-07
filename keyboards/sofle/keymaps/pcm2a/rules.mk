96aafc24a8d34cbe97748c12fc13e932ccfcd1c0
Cameron McBride <c@c.com>
2024-12-07 10:06:41 -0600

    sofle updates for animations

--- keyboards/sofle/keymaps/pcm2a/rules.mk
+++ keyboards/sofle/keymaps/pcm2a/rules.mk
@@ -4,5 +4,7 @@ ENCODER_MAP_ENABLE = yes
 CONSOLE_ENABLE = no
 RGB_MATRIX_CUSTOM_USER = no
 TAP_DANCE_ENABLE = yes
+WPM_ENABLE = yes
 SRC += starfield.c
 # SRC += matrix.c
+CFLAGS += -Wno-error=unused-function
