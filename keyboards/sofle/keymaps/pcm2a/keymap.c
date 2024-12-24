564c2911da8aa389c37d30b320cc06cede712178
Cameron McBride <c@c.com>
2024-12-24 14:10:37 -0600

    Updates

--- keyboards/sofle/keymaps/pcm2a/keymap.c
+++ keyboards/sofle/keymaps/pcm2a/keymap.c
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_1,     KC_2,   KC_3,    KC_4,    KC_5,    KC_6,                     KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,   KC_BSPC,
   TD(TD_TAB_GRV),   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  TD(TD_LBRC_BSLS),
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX,KC_N,    KC_M, TD(TD_UP_COMM),  KC_DOT, KC_SLSH,  MT(MOD_RSFT, KC_RBRC),
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LGUI,KC_LALT,MO(2),KC_SPC, KC_ENT,      KC_ESC,  KC_SPC, KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 /*
@@ -175,7 +175,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_EQL,
   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
   _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,      RGB_TOGX, KC_LBRC, KC_RBRC, KC_COMM, KC_COLN, KC_BSLS, _______,
+  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,      RGB_TOGX, _______, KC_RBRC, KC_UP, KC_COLN, KC_BSLS, KC_RBRC,
                        QK_CLEAR_EEPROM, _______, _______, TG(4), _______,       _______, KC_BSLS, KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 /* RAISE
@@ -214,10 +214,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [4] = LAYOUT(
-  _______, _______ , _______ , _______ , _______ , _______,                         KC_1,   KC_2,   KC_3,   KC_4,   KC_5,    _______,
+  _______, _______ , _______ , _______ , _______ , _______,                         KC_1,   KC_2,   KC_3,   KC_4,   KC_5,    KC_B,
   _______,  _______,  _______,   _______,  _______, _______,                        KC_R,   KC_E,   KC_W,   KC_Q,   KC_TAB,  KC_M,
-  _______, _______,  _______,  _______,  _______, _______,                          KC_F,   KC_A,   KC_S,   KC_D,   KC_LSFT, KC_LCTL,
-  _______,_______, _______, _______, _______, _______,  _______,       _______,     KC_V,   KC_C,   KC_X,   KC_Z,   KC_LSFT, KC_LCTL,
+  _______, _______,  _______,  _______,  _______, _______,                          KC_F,   KC_A,   KC_S,   KC_D,   KC_LSFT, KC_LSFT,
+  _______,_______, _______, _______, _______, _______,  _______,       _______,     KC_X,   KC_M,   KC_V,   _______, KC_LCTL, KC_LCTL,
                          _______, _______, _______, _______, _______,       KC_ESC, KC_SPC, KC_SPC, _______, _______
 ),
 /* Arrow Keys
@@ -779,7 +779,7 @@ static void render_cat_anim(void) {
         anim_sleep = timer_read32();
     } else {
         if(timer_elapsed32(anim_sleep) > OLED_TIMEOUT) {
-            oled_off();
+            // oled_off();
         } else {
             if(timer_elapsed32(cat_anim_timer) > ANIM_FRAME_DURATION) {
                 cat_anim_timer = timer_read32();
